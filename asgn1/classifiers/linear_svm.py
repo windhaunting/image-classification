0f7bd88bf69095abc43fe9cffdec68c6893f4e10
fubao <freadream@gmail.com>
2018-01-06 23:56:49 -0500

    update_svm

--- asgn1/classifiers/linear_svm.py
+++ asgn1/classifiers/linear_svm.py
@@ -25,23 +25,26 @@ def svm_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
-  for i in xrange(num_train):
+  for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
+    for j in range(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+ 	dW[:,y[i]] -= X[i,:]
+	dW[:,j] += X[i,:] 
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
-
+	
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
